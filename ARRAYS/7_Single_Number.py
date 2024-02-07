ca82ee115296fd2a8fcb08346e3859a4a8b4aa63
uday <uvs2111@gmail.com>
2024-02-07 19:14:14 +0530

    python basics

--- ARRAYS/7_Single_Number.py
+++ ARRAYS/7_Single_Number.py
@@ -1,19 +1,31 @@
 def singlenumber(arr):
     # O(n)/O(n)
-    dicti={}
+    """approach-1"""
+    # dicti={}
+    #
+    # for ele in arr:
+    #     if ele not in dicti:
+    #         dicti[ele]=1
+    #     else:
+    #         dicti[ele]+=1
+    #
+    # for key in dicti:
+    #     if dicti[key]==1:
+    #         # ans.append(key)
+    #         return key
+    #
+    # return -1
 
+    """Optimised approach"""
+    # O(n)/O(1)
+    ans = 0
+    # ele^ele=0
+    # ele^0=ele
+    # duplicate elements gets cancelled out and single element stores in ans
     for ele in arr:
-        if ele not in dicti:
-            dicti[ele]=1
-        else:
-            dicti[ele]+=1
+        ans = ans ^ ele
 
-    for key in dicti:
-        if dicti[key]==1:
-            # ans.append(key)
-            return key
+    return ans
 
-    return -1
-
-nums = [4,1,2,1,2,4]
+nums = [4,1,2,1,2]
 print(singlenumber(nums))

432b12a447989ac3c830ef9aacdecc4bdb4dbad1
uday <uvs2111@gmail.com>
2024-02-07 18:41:43 +0530

    python basics

--- ARRAYS/6_Duplicates_Array.py
+++ ARRAYS/6_Duplicates_Array.py
@@ -1,6 +1,30 @@
 def duplicate(arr):
-    pass
+
+    arr.sort() # O(nlogn)
+    n=len(arr)
+    for i in range(n-1): # O(n)
+        if arr[i]==arr[i+1]:
+            return True
+
+    return False
+
+
+def duplicate1(arr):
+    # O(n)/O(n)
+    dicti={}
+    # {arr[i]:frequency}
+    for ele in arr:
+        if ele not in dicti: # agr pehli baar aa rha to dict me daal do
+            dicti[ele]=1
+        else: # agr doosri baar aa rha to True return kr do
+            # dicti[ele]+=1
+            # if dicti[ele]==2:
+            #     return True
+            return True
+
+    return False
 
 
 
-arr=[1,1,1,3,3,4,3,2,4,2]
+arr=[1,2,3,0,4,1]
+print(duplicate1(arr))

fa08e6b6826d62441771af7f4621d88fb4d4ea34
uday <uvs2111@gmail.com>
2024-02-13 11:10:47 +0530

    initial commit

--- RECURSION/Sum of n Natural Numbers.py
+++ RECURSION/Sum of n Natural Numbers.py
@@ -10,3 +10,12 @@ def sumn(total,i,n):
 total=[0]
 print(sumn(total,1,5))
 print(total[0])
+
+
+def fun(n):
+    # jab first natural number ka sum mnga ja rha hai to 1 return kr do
+    if n==1: return 1
+    else:
+        return n+fun(n-1)
+
+print(fun(7))
